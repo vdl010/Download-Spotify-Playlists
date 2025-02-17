c4046e0d6db6844f59bf865e92d83207425ece39
Jan-Michael van der Linde <108940700+vdl010@users.noreply.github.com>
2025-02-17 22:11:10 +0100

    Update Endpoints.java

--- Spotify_to_MP3/src/main/java/Endpoints.java
+++ Spotify_to_MP3/src/main/java/Endpoints.java
@@ -1,5 +1,5 @@
 public class Endpoints {
     public static final String CLIENT_ID = "3e76638b45964622b75e5afb7e05d2b4";
-    public static final String CLIENT_SECRET = "a3a5b61ebd00404089056ad002c47ca8";
+    public static final String CLIENT_SECRET = "";
     public static final String TOKEN = "https://accounts.spotify.com/api/token";
 }
